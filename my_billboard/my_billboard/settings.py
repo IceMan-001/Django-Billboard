c299d352bec35cabbcbcab7d174e715ee25aa8d0
Andrey Bobrov <8867006@mail.ru>
2024-10-19 20:38:54 +0300

    доработана функция смены пароля

--- my_billboard/my_billboard/settings.py
+++ my_billboard/my_billboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oe#en$d*&5jnk)v19amb3c6&%+lc@-^@p8ekep3vd^4n4y2=@4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'my_billboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_billboard',
-        'USER': 'django_admin',
-        'PASSWORD': '4321',
-        'HOST': '127.0.0.1',
-        'PORT': 5433,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_billboard',
+#         'USER': 'django_admin',
+#         'PASSWORD': '4321',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5433,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- my_billboard/users/forms.py
+++ my_billboard/users/forms.py
@@ -53,3 +53,25 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User  # Установка связи между model и User
         fields = ('username', 'first_name', 'last_name', 'email', 'phone', 'city', 'image')
+
+
+class ChangePasswordForm(forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput, label='Старый пароль')
+    new_password = forms.CharField(widget=forms.PasswordInput, label='Новый пароль')
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label='Подтверждение нового пароля')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        old_password = cleaned_data.get('old_password')
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        # Проверка на совпадение нового пароля со старым
+        if old_password == new_password:
+            self.add_error('new_password', 'Новый пароль не должен совпадать со старым.')
+
+        # Проверка на совпадение нового пароля с подтверждением
+        if new_password != confirm_password:
+            self.add_error('confirm_password', 'Пароли не совпадают.')
+
+        return cleaned_data
